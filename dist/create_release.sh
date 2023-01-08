9ec95cd69a9222f9d9edcd6c54bfa69e86d5d8ee
Trevor Gross <tmgross@umich.edu>
2023-01-07 22:00:36 -0500

    Correct workflow

--- dist/create_release.sh
+++ dist/create_release.sh
@@ -24,8 +24,8 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
-mkdir -p "$staging/completion"
-mkdir -p "$staging/doc"
+mkdir -p "${staging}/completion"
+mkdir -p "${staging}/doc"
 
 # Remove the "unreleased" section from our changelog
 perl -0777 -pe "s/(<\!-- next-header -->.*## \[Unreleased\].*?\n)(?=## |<\!--)//gms" CHANGELOG.md > CHANGELOG.md.dist
@@ -42,50 +42,28 @@ cat RELNOTES.md
 
 # Create Makefile.dist for shipping with the package
 
-cp README.md "$staging/"
-cp LICENSE "$staging/"
-cp Makefile.dist "$staging/Makefile"
-cp CHANGELOG.md.dist "$staging/doc/CHANGELOG.md"
-
-cat << EOF >> "$staging/INSTALLING"
-msgpack: CLI MessagePack utility
-
-To install this program, run 'make install' and to remove, run
-'make uninstall'. To install to a different location, specify the `PREFIX`
-environment variable.
-
-$ # install the program
-$ make install
-
-$ # uninstall the program
-$ make uninstall
-
-$ # install or uninstall in a specific location
-$ PREFIX="~/local" make install
-
-Alternatively, just copy the binary to your desired location
-
-If you would like, you can validate binaries before running:
-
-$ shasum -a 256 -c sha256sum
-EOF
+cp README.md "${staging}/"
+cp LICENSE "${staging}/"
+cp Makefile.dist "${staging}/Makefile"
+cp CHANGELOG.md.dist "${staging}/doc/CHANGELOG.md"
+cp dist/INSTALLING "${staging}/"
 
 # Copy manpages
-cp "$assetdir"/msgpack.1 "$staging/doc"
+cp "${assetdir}/msgpack.1" "${staging}/doc"
 
 completion_scripts="_msgpack _msgpack.ps1 msgpack.bash msgpack.elv msgpack.fish"
 for script in $completion_scripts; do
-    cp "$assetdir/$script" "$staging/completion"
+    cp "${assetdir}/${script}" "${staging}/completion"
 done
 
 if [ "$machine" = "cygwin" ]; then
-    cp "$builddir/release/msgpack.exe" "$staging/"
-    sha256sum staging/msgpack.exe | perl -pe "s/$staging\///" >> "$staging/sha256sum"
-    7z a "$staging.zip" "$staging"
-    echo "ASSET=$staging.zip" >> "$GITHUB_ENV"
+    cp "${builddir}/msgpack.exe" "${staging}/"
+    sha256sum staging/msgpack.exe | perl -pe "s/${staging}\///" >> "${staging}/sha256sum"
+    7z a "${staging}.zip" "$staging"
+    echo "ASSET=${staging}.zip" >> "$GITHUB_ENV"
 else
-    cp "$builddir/release/msgpack" "$staging/"
-    shasum -a 256 staging/msgpack | perl -pe "s/$staging\///" >> "$staging/sha256sum"
-    tar czf "$staging.tar.gz" "$staging"
-    echo "ASSET=$staging.tar.gz" >> "$GITHUB_ENV"
+    cp "${builddir}/msgpack" "$staging/"
+    shasum -a 256 staging/msgpack | perl -pe "s/${staging}\///" >> "${staging}/sha256sum"
+    tar czf "${staging}.tar.gz" "$staging"
+    echo "ASSET=${staging}.tar.gz" >> "$GITHUB_ENV"
 fi
